2b6fa5ca34cb94c9ba6460ce077347d4b7269657
bitnet-io <123344793+bitnet-io@users.noreply.github.com>
2023-12-31 04:24:04 -0700

    Delete radiocoin-wallet-bundle-current-09-22-2021 directory

--- radiocoin-wallet-bundle-current-09-22-2021/recreate-keystore.sh
+++
@@ -1,1 +0,0 @@
-openssl pkcs12 -inkey upload_cert -in deployment_ -export -out keys.pkcs12
--- radiocoin-wallet-bundle-current-09-22-2021/sign-AAB.sh
+++
@@ -1,1 +0,0 @@
-keytool -genkeypair -alias upload -keyalg RSA -keysize 2048 -validity 9125 -keystore keystore.jks
